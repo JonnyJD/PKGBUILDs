366219f0853c2a50b8d7b44f972141cffd54c405
Johannes Dewender <arch@JonnyJD.net>
2015-06-08 19:31:53 +0200

    _bin: aur4_import.sh run commands now
    also add --force to filter branch

--- _bin/aur4_import.sh
+++ _bin/aur4_import.sh
@@ -1,5 +1,5 @@
 if [ $# -lt 1 -o \( "$1" = "-h" -o "$1" = "--help" \) ]; then
-	echo usage: $0 <path/to/project>
+	echo "usage: $0 <path/to/project>"
 	echo "(has to be started from root of the existing repository)"
 	exit -1
 fi
@@ -8,9 +8,11 @@ prefix=$1
 pkg=`basename $prefix`
 gitignore=`pwd`/gitignore-pkg
 
-echo git subtree split --prefix="$prefix" -b aur4/$pkg
-echo git filter-branch --tree-filter "cp $gitignore .gitignore; mksrcinfo" \
-	-- aur4/$pkg
+git subtree split --prefix="$prefix" -b aur4/$pkg || exit -1
+git filter-branch -f --tree-filter "cp $gitignore .gitignore; mksrcinfo" \
+	-- aur4/$pkg || exit -1
 
-echo ssh aur@aur4.archlinux.org setup-repo $pkg
-echo git push ssh+git://aur@aur4.archlinux.org/${pkg}.git/ aur4/$pkg:master
+#ssh aur@aur4.archlinux.org setup-repo $pkg
+git push ssh+git://aur@aur4.archlinux.org/${pkg}.git/ aur4/$pkg:master \
+	|| exit -1
+git branch -D aur4/$pkg
