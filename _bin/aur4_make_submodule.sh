36a5203e63fb49b4f79f8b77ee0cba2daa9e1ac2
Johannes Dewender <arch@JonnyJD.net>
2015-06-09 15:24:18 +0200

    separate pushurl for submodules
    fetch operations don't need ssh/write access
    and can be done without authentication

--- _bin/aur4_make_submodule.sh
+++ _bin/aur4_make_submodule.sh
@@ -1,9 +1,12 @@
+#!/bin/bash
+
 if [ $# -lt 1 -o \( "$1" = "-h" -o "$1" = "--help" \) ]; then
 	echo "usage: $0 <path/to/project>"
+	echo "(should be done in root of repository)"
 	exit -1
 fi
 
-path=$1
+path=${1%/}
 pkg=`basename $path`
 email=`git config user.email`
 
@@ -14,7 +17,16 @@ echo mv \"$path\" \"${pkg}_aur4_bak\"
 mv "$path" "${pkg}_aur4_bak"
 
 echo git submodule add ssh+git://aur@aur4.archlinux.org/${pkg}.git $path
-git submodule add ssh+git://aur@aur4.archlinux.org/${pkg}.git $path
+git submodule add https://aur@aur4.archlinux.org/${pkg}.git $path
+
+# for this we need to be in the root of the rppository
+echo git config --file=.gitmodules submodule.{$path}.pushurl \
+	ssh+git://aur@aur4.archlinux.org/${pkg}.git $path
+git config --file=.gitmodules submodule.{$path}.pushurl \
+	ssh+git://aur@aur4.archlinux.org/${pkg}.git $path
+
+echo git submodule sync
+git submodule sync
 
 echo cp -r \"${pkg}_aur4_bak/\"* \"$path\"
 cp -r "${pkg}_aur4_bak/"* "$path"
